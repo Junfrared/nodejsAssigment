7d266e03028302f0b389880a87262afe6c11861b
Junaid <syedm.junaid@hotmail.com>
2022-08-20 00:53:33 +0500

    form submission with routes

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
+//importing express
 const express = require("express");
+
+//creating server
 const app = express();
 
+//Adding Middleware for parsing requestBody
+app.use(express.urlencoded({ extended: false }));
+
+//Creating Routes
 app.get("/", (req, res) => {
-    console.log("sending response");
-    res.send(`<h1>My first response</h1>`);
+    console.log("my first response");
+    res.send(`<h1>This is my first response</h1>`);
 });
 
-app.get("/second", (req, res) => {
-    res.send(`<h1>My second response</h1>
-                <p>This is my paragraph</p>
+app.get("/2nd", (req, res) => {
+    console.log("my second response");
+    res.send(`<h2>This is my second response ${res}</h2>`);
+});
+
+app.route("/form")
+    .get((req, res) => {
+        res.status(200).send(`
+        <form action = '/form' method = 'POST'>
+        <label>First Name</label><br>
+        <input type = "text" name="fname" placeholder = "type your 1st name"><br><br>
+
+        <label>Last Name</label><br>
+        <input type ="text" name = "lname" placeholder = 'type your last name'><br>
+        <br>
+        <label>Email</label><br>
+        <input type = 'email' name = 'email' placeholder = 'type a valid email'><br>
+        <br>
+        <label>Phone</label><br>
+        <input type = 'tel' name = 'phone' placeholder = '000-000-0000' pattern = '[0-9]{3}-[0-9]{2}-[0-9]{4}'><br>
+        <label>Age</label><br>
+        <input type = 'number' name = 'age' placeholder = '00' min = '0' max = '100'><br>
+        <br>
+        <input type = 'password' name = 'pass' placeholder = 'password'><br>
+        <br>
+        <button type = 'submit'>Submit</button>
+
+
+        </form>
+        
+
     `);
-});
-app.get("/third", (req, res) => {
-    console.log("this is thirdone");
-    res.send(`<h1>This is my third Response</h1>
-            <p>This is my image</p>
-            <img src="https://tdeecalculator.net/assets/images/food-scale.jpg">
-    `);
-});
+    })
+    .post((req, res) => {
+        console.log(req.body);
+        res.status(200).send(req.body.fname);
+    });
 
-app.use((req, res) => {
-    res.status(404).send("This is my 404 response");
+//creating 404 route
+app.use((req, res, next) => {
+    console.log("404");
+    res.status(404).send(`<h3>this my third ${res}</h3>`);
 });
-
+//making server live at port 3000
 app.listen(3000);
